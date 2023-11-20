7b1dfa7bc888aba75ca220d0d8427da19dbe5c70
Kim, Isu <49092508+isu-kim@users.noreply.github.com>
2023-11-21 04:16:11 +0900

    Add UDP support
    - feature: almost forgot how UDP worked :(

--- hw1-lb/server/server.go
+++ hw1-lb/server/server.go
@@ -15,12 +15,13 @@ import (
 
 // Server represents a single server
 type Server struct {
-	listener net.Listener
-	address  string
-	port     int
-	proto    uint8
-	alias    string
-	stopper  chan os.Signal
+	tcpListener net.Listener
+	udpConn     net.Conn
+	address     string
+	port        int
+	proto       uint8
+	alias       string
+	stopper     chan os.Signal
 }
 
 // ConnectionHandler is a callback function for handling connections
@@ -28,13 +29,6 @@ type ConnectionHandler func(conn net.Conn)
 
 // New creates a new Server
 func New(addr string, port int, proto string, alias string) (*Server, error) {
-	listenAddr := fmt.Sprintf("%s:%d", addr, port)
-	listener, err := net.Listen(proto, listenAddr)
-	if err != nil {
-		msg := fmt.Sprintf("could not start server %s/%s: %v", proto, addr, err)
-		return nil, errors.New(msg)
-	}
-
 	// Convert proto from string to uint8
 	var protoConverted uint8
 	if strings.Compare(proto, "tcp") == 0 {
@@ -43,55 +37,107 @@ func New(addr string, port int, proto string, alias string) (*Server, error) {
 		protoConverted = common.TypeProtoUDP
 	}
 
-	// Return Server
-	return &Server{
-		listener: listener,
-		address:  addr,
-		port:     port,
-		proto:    protoConverted,
-		alias:    alias,
-		stopper:  make(chan os.Signal),
-	}, nil
+	listenAddr := fmt.Sprintf("%s:%d", addr, port)
+
+	// If this is a TCP server
+	if protoConverted == common.TypeProtoTCP {
+		listener, err := net.Listen(proto, listenAddr)
+		if err != nil {
+			msg := fmt.Sprintf("could not start server %s/%s: %v", proto, addr, err)
+			return nil, errors.New(msg)
+		}
+
+		// Return Server
+		return &Server{
+			tcpListener: listener,
+			address:     addr,
+			port:        port,
+			proto:       protoConverted,
+			alias:       alias,
+			stopper:     make(chan os.Signal),
+		}, nil
+	} else if protoConverted == common.TypeProtoUDP {
+		// This is a UDP server
+		server, err := net.ListenUDP("udp", &net.UDPAddr{
+			IP:   net.ParseIP(addr),
+			Port: port,
+		})
+
+		if err != nil {
+			msg := fmt.Sprintf("could not start server %s/%s: %v", proto, addr, err)
+			return nil, errors.New(msg)
+		}
+
+		return &Server{
+			udpConn: server,
+			address: addr,
+			port:    port,
+			proto:   protoConverted,
+			alias:   alias,
+			stopper: make(chan os.Signal),
+		}, nil
+	} else {
+		return nil, errors.New("unknown protocol")
+	}
 }
 
 // Close stops listening a Server
 func (s *Server) Close() error {
 	signal.Notify(s.stopper, os.Interrupt)
-	return s.listener.Close()
+	if s.proto == common.TypeProtoTCP {
+		return s.tcpListener.Close()
+	} else if s.proto == common.TypeProtoUDP {
+		return s.udpConn.Close()
+	} else {
+		return errors.New("unknown protocol")
+	}
 }
 
 // DoMainLoop loops forever and accepts connections
 // The sync.WaitGroup will tell this goroutine when to stop listening
 // The ConnectionHandler will tell which function to call upon a connection
 func (s *Server) DoMainLoop(wg *sync.WaitGroup, handler ConnectionHandler) {
-	for {
-		select {
-		case <-s.stopper:
-			log.Printf("%s Server %s/%s:%d received interrupt",
-				common.ColoredInfo, misc.ConvertProtoToString(s.proto), s.address, s.port)
-			return
-		default:
-			// Accept a new connection
-			conn, err := s.listener.Accept()
-			if err != nil {
-				// Check if the error is due to the listener being closed
-				_, ok := err.(net.Error)
-				if ok {
-					continue
+	if s.proto == common.TypeProtoTCP {
+		for {
+			select {
+			case <-s.stopper:
+				log.Printf("%s Server %s/%s:%d received interrupt",
+					common.ColoredInfo, misc.ConvertProtoToString(s.proto), s.address, s.port)
+				return
+			default:
+				// Accept a new connection
+				conn, err := s.tcpListener.Accept()
+				if err != nil {
+					// Check if the error is due to the tcpListener being closed
+					_, ok := err.(net.Error)
+					if ok {
+						continue
+					}
+
+					// Listener is closed or other non-temporary error
+					log.Printf("%s \"%s(%s/%s)\" Error accepting connection: %v",
+						common.ColoredWarn, s.alias, s.proto, s.address, err)
+					return
 				}
 
-				// Listener is closed or other non-temporary error
-				log.Printf("%s \"%s(%s/%s)\" Error accepting connection: %v",
-					common.ColoredWarn, s.alias, s.proto, s.address, err)
-				return
+				// Handle the connection in a new goroutine
+				/**
+				log.Printf("%s \"%s(%s/%s)\" Request from: %s",
+					common.ColoredInfo, s.alias, misc.ConvertProtoToString(s.proto), s.address, conn.RemoteAddr())
+				*/
+				go handler(conn)
 			}
-
-			// Handle the connection in a new goroutine
-			/**
-			log.Printf("%s \"%s(%s/%s)\" Request from: %s",
-				common.ColoredInfo, s.alias, misc.ConvertProtoToString(s.proto), s.address, conn.RemoteAddr())
-			*/
-			go handler(conn)
+		}
+	} else if s.proto == common.TypeProtoUDP {
+		for {
+			select {
+			case <-s.stopper:
+				log.Printf("%s Server %s/%s:%d received interrupt",
+					common.ColoredInfo, misc.ConvertProtoToString(s.proto), s.address, s.port)
+				return
+			default:
+				handler(s.udpConn)
+			}
 		}
 	}
 }
