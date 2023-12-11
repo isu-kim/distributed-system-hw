253089192f7ff158ffca6967c7f8cdd30acde39b
Kim, Isu <49092508+isu-kim@users.noreply.github.com>
2023-12-12 05:08:32 +0900

    remote write works

--- hw2-ds/seph-server/api/remote.go
+++ hw2-ds/seph-server/api/remote.go
@@ -53,10 +53,119 @@ func (h *Handler) remoteForwardPrimary(c *gin.Context) {
 		return
 	}
 
+	// Till here, only the primary knows that a note was written
+	// Serialize the payload to JSON
+	payloadBytes, err := json.Marshal(newNote)
+	if err != nil {
+		errResponse := common.NoteErrorResponse{
+			Msg:    err.Error(),
+			Method: c.Request.Method,
+			Uri:    c.Request.RequestURI,
+			Body:   fmt.Sprintf("%v", newNote),
+		}
+
+		// Return bad request, user sent us bad thing!
+		c.JSON(http.StatusInternalServerError, errResponse)
+		log.Printf("%s [REPLY][%s] %s %v",
+			misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+		return
+	}
+
+	// Now primary shall tell all replicas to update
+	// For all replicas, update
+	for i, replica := range h.replicas {
+		if i == 0 { // Skip current replica
+			continue
+		}
+
+		// Perform request accordingly
+		var response *http.Response
+		endpoint := fmt.Sprintf("http://%s/backup", replica)
+		log.Printf("[DEBUG] propagating to %s", replica)
+		if strings.Contains(c.Request.Method, "POST") { // Forward POST
+			// Create POST Request
+			response, err = http.Post(endpoint, "application/json", bytes.NewBuffer(payloadBytes))
+			if err != nil {
+				log.Printf("Error making POST request to replica %s: %v\n", endpoint, err)
+				errResponse := common.NoteErrorResponse{
+					Msg:    err.Error(),
+					Method: c.Request.Method,
+					Uri:    c.Request.RequestURI,
+					Body:   fmt.Sprintf("%v", reqNote),
+				}
+
+				// Return bad request, user sent us bad thing!
+				c.JSON(http.StatusInternalServerError, errResponse)
+				log.Printf("%s [REPLY][%s] %s %v",
+					misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+				return
+			}
+		} else if strings.Contains(c.Request.Method, "PUT") ||
+			strings.Contains(c.Request.Method, "PATCH") { // Forward PUT or PATCH
+
+			// Create a new request accordingly
+			request, err := http.NewRequest(c.Request.Method, endpoint, bytes.NewBuffer(payloadBytes))
+			if err != nil {
+				log.Printf("Error creating %s request: %v to replica %s", c.Request.Method, endpoint, err)
+				errResponse := common.NoteErrorResponse{
+					Msg:    err.Error(),
+					Method: c.Request.Method,
+					Uri:    c.Request.RequestURI,
+					Body:   fmt.Sprintf("%v", reqNote),
+				}
+
+				// Return bad request, user sent us bad thing!
+				c.JSON(http.StatusInternalServerError, errResponse)
+				log.Printf("%s [REPLY][%s] %s %v",
+					misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+				return
+			}
+
+			request.Header.Set("Content-Type", "application/json")
+			// Perform the request
+			client := http.Client{}
+			response, err = client.Do(request)
+			if err != nil {
+				log.Printf("Error making %s request to replica %s: %v\n", c.Request.Method, endpoint, err)
+				errResponse := common.NoteErrorResponse{
+					Msg:    err.Error(),
+					Method: c.Request.Method,
+					Uri:    c.Request.RequestURI,
+					Body:   fmt.Sprintf("%v", reqNote),
+				}
+
+				// Return bad request, user sent us bad thing!
+				c.JSON(http.StatusInternalServerError, errResponse)
+				log.Printf("%s [REPLY][%s] %s %v",
+					misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+				return
+			}
+		} // I sincerely will ignore the exceptional cases
+
+		// Check status code from the /backup API
+		if response.StatusCode == http.StatusOK {
+			continue
+		} else {
+			log.Printf("Error updating POST request to replica %s: %v\n", endpoint, err)
+			errResponse := common.NoteErrorResponse{
+				Msg:    fmt.Sprintf("replica %s failed to update", replica),
+				Method: c.Request.Method,
+				Uri:    c.Request.RequestURI,
+				Body:   fmt.Sprintf("%v", reqNote),
+			}
+
+			// Return bad request, user sent us bad thing!
+			c.JSON(http.StatusInternalServerError, errResponse)
+			log.Printf("%s [REPLY][%s] %s %v",
+				misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+			return
+		}
+
+		response.Body.Close()
+	}
+
 	// Everything went on correct
 	c.JSON(http.StatusOK, newNote)
-
-	// Till here, only the primary knows that a note was written
 }
 
 // remoteDeletePrimary is for [DELETE] /primary/{0-9} API
@@ -92,23 +201,146 @@ func (h *Handler) remoteDeletePrimary(c *gin.Context) {
 		response.Msg = "FAILED"
 		c.JSON(http.StatusInternalServerError, response)
 		return
+	}
+
+	// Till here, only primary knows that a note was deleted
+
+	// Now primary shall tell all replicas to update
+	// For all replicas, delete
+	for i, replica := range h.replicas {
+		if i == 0 { // Skip current replica
+			continue
+		}
+
+		// Perform request accordingly
+		endpoint := fmt.Sprintf("http://%s/backup/%d", replica, id)
+
+		// Create a new request accordingly
+		request, err := http.NewRequest("DELETE", endpoint, nil)
+		if err != nil {
+			log.Printf("Error creating %s request: %v to replica %s", c.Request.Method, endpoint, err)
+
+			response.Msg = "FAILED"
+			c.JSON(http.StatusInternalServerError, response)
+			log.Printf("%s [REPLY][%s] %s %v",
+				misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, response)
+			return
+		}
+		request.Header.Set("Content-Type", "application/json")
+
+		// Perform the request
+		client := http.Client{}
+		res, err := client.Do(request)
+		if err != nil {
+			log.Printf("Error making %s request to replica %s: %v\n", c.Request.Method, endpoint, err)
+
+			response.Msg = "FAILED"
+			c.JSON(http.StatusInternalServerError, response)
+			log.Printf("%s [REPLY][%s] %s %v",
+				misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, response)
+			return
+		}
+
+		// Check status code from the /backup API
+		if res.StatusCode == http.StatusOK {
+			continue
+		} else {
+			log.Printf("Error upating DELETE request to replica %s: %v\n", endpoint, err)
+
+			response.Msg = "FAILED"
+			c.JSON(http.StatusInternalServerError, response)
+			log.Printf("%s [REPLY][%s] %s %v",
+				misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, response)
+			return
+		}
+
+		res.Body.Close()
+	}
+
+	// Yes, the update went on correctly!
+	response.Msg = "OK"
+	c.JSON(http.StatusOK, response)
+	log.Printf("%s [REPLY][%s] %s %v",
+		misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, response)
+	return
+}
+
+// remoteUpdateBackup is for [POST/PUT/PATCH] /backup API
+func (h *Handler) remoteUpdateBackup(c *gin.Context) {
+	// Try parsing body JSON
+	err, reqNote := clientRequest(c)
+	if err != nil {
+		errResponse := common.NoteErrorResponse{
+			Msg:    err.Error(),
+			Method: c.Request.Method,
+			Uri:    c.Request.RequestURI,
+			Body:   "", // When json parsing failed, we regard body as empty
+		}
+
+		// Return bad request, user sent us bad thing!
+		c.JSON(http.StatusBadRequest, errResponse)
+		log.Printf("%s [REPLY][%s] %s %v",
+			misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+		return
+	}
+
+	// Perform remote write
+	err, newNote := h.performRemoteWrite(c, reqNote)
+	if err != nil {
+		errResponse := common.NoteErrorResponse{
+			Msg:    err.Error(),
+			Method: c.Request.Method,
+			Uri:    c.Request.RequestURI,
+			Body:   fmt.Sprintf("%v", reqNote),
+		}
+
+		// Return bad request, user sent us bad thing!
+		c.JSON(http.StatusInternalServerError, errResponse)
+		log.Printf("%s [REPLY][%s] %s %v",
+			misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+		return
+	}
+
+	// This replica's update was successful
+	c.JSON(http.StatusOK, newNote)
+	return
+}
+
+// remoteDeleteBackup is for [DELETE] /backup/{0-9} API
+func (h *Handler) remoteDeleteBackup(c *gin.Context) {
+	// Read ID param from API
+	noteID := c.Param("id")
+
+	// ID was unable to be converted as an integer
+	id, err := strconv.Atoi(noteID)
+	if err != nil {
+		errResponse := common.NoteErrorResponse{
+			Msg:    "wrong URI, ID was invalid",
+			Method: c.Request.Method,
+			Uri:    c.Request.RequestURI,
+			Body:   "",
+		}
+
+		c.JSON(http.StatusBadRequest, errResponse)
+		log.Printf("%s [REPLY][%s] %s %v",
+			misc.ColoredReplica, c.Request.Method, c.Request.RequestURI, errResponse)
+		return
+	}
+
+	// Perform note delete
+	err = h.performRemoteDelete(id)
+	response := struct {
+		Msg string `json:"msg"`
+	}{}
+	if err != nil {
+		response.Msg = "FAILED"
+		c.JSON(http.StatusInternalServerError, response)
+		return
 	} else {
 		response.Msg = "OK"
 		c.JSON(http.StatusOK, response)
 		return
 	}
-
-	// Till here, only primary knows that a note was deleted
-}
-
-// remoteUpdateBackup is for [POST/PUT/PATCH] /backup API
-func (h *Handler) remoteUpdateBackup(c *gin.Context) {
-
-}
-
-// remoteDeleteBackup is for [DELETE] /backup/{0-9} API
-func (h *Handler) remoteDeleteBackup(c *gin.Context) {
-
 }
 
 // handleRemoteWrite handles remote write
@@ -226,7 +458,7 @@ func (h *Handler) performRemoteWrite(c *gin.Context, note common.Note) (error, c
 			return err, common.Note{}
 		}
 
-		return nil, common.Note{}
+		return nil, original
 	} else if strings.Contains(c.Request.Method, "PUT") {
 		// First, find original note
 		err, original := h.dsh.ReadSpecific(note.Id)
@@ -246,7 +478,7 @@ func (h *Handler) performRemoteWrite(c *gin.Context, note common.Note) (error, c
 			return err, common.Note{}
 		}
 
-		return nil, common.Note{}
+		return nil, original
 	} else {
 		return errors.New("unknown method"), common.Note{}
 	}
